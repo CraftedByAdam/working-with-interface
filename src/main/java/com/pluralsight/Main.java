6d6ba4c88d381a35a848f1752d5121d709c88751
Your Name <youremail@example.com>
2026-05-14 15:23:45 -0400

    I built the 'Person' class, I made the 'compareTo' method with the help of the documents provided. Tested it in my Main.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,23 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Person> myFamily = new ArrayList<Person>();
+        myFamily.add(new Person("Dana", "Wyatt", 63));
+        myFamily.add(new Person("Zachary", "Westly", 31));
+        myFamily.add(new Person("Elisha", "Aslan", 14));
+        myFamily.add(new Person("John", "Herbert", 16));
+        myFamily.add(new Person("Brooklyn", "Madison", 9));
+        myFamily.add(new Person("Zara", "Nodes", 19));
+
+        Collections.sort(myFamily);
+
+        for (Person person : myFamily) {
+            System.out.println(person);
+        }
+    }
 }
--- src/main/java/com/pluralsight/Person.java
+++ src/main/java/com/pluralsight/Person.java
@@ -1,4 +1,32 @@
 package com.pluralsight;
 
-public class Person {
+public class Person implements Comparable<Person>{
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+
+    @Override
+    public int compareTo(Person person) {
+        int lastNameCompare = this.lastName.compareTo(person.lastName);
+        if(lastNameCompare != 0){
+            return lastNameCompare;
+        }
+        int firstNameCompare = this.firstName.compareTo(person.firstName);
+        if(firstNameCompare != 0){
+            return firstNameCompare;
+        }
+        return Integer.compare(this.age, person.age);
+    }
+
+    @Override
+    public String toString() {
+        return lastName + " " + firstName + ", " + age;
+    }
 }
